e1529404f1a8e26a5a406bef564d23f3925aded1
merey90 <merey.90@gmail.com>
2015-11-05 10:29:41 +0000

    logo change, some trash clear, fix homepage

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -32,7 +32,7 @@ module SessionsHelper
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
-    !current_user.nil?
+    !current_user.nil? && current_user.admin?
   end
   
   # Forgets a persistent session.
